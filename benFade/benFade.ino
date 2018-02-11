a0fd7e1d41670be96f078f67a8d0dca05d30b281
Dave Barnett <dave.barnett@redsensors.com>
2018-02-11 13:15:36 +0000

    Refactored and commented version.

--- benFade/benFade.ino
+++ benFade/benFade.ino
@@ -1,57 +1,63 @@
 #include "FastLED.h"
 
-#define NUM_LEDS 1
 #define DATA_PIN 0
 
-#ifdef ARDUINO_AVR_UNO
-#define SERIAL_LOGGING
-#endif
-
-CRGB leds[NUM_LEDS];
-static uint8_t heatIndex = 0;
-const byte buttonPin = 13;
-uint8_t headIndexStart = 90;
+const byte ledPin = 0;  // Pin on which LED data is send
+const byte buttonPin = 3; // Pin button is connected to
+CRGB led; // current LED color, refenced by FastLED library
+uint8_t glowIndex = 0;  // current index into palette of glow colour
+const uint8_t glowStart = 90; // Starting index into palette that glow starts
+const uint32_t glowDuration = 20;  // Number of seconds from start to finish of glow
+const uint32_t interval = (glowDuration * 1000) / glowStart;  // Time between glow index changes to achieve glow duration
 
 void setup()
 {
-#ifdef SERIAL_LOGGING
-  Serial.begin(9600);
-  Serial.println("Serial Logging Enabled");
-#endif
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  // Configure LED and button
+  FastLED.addLeds<NEOPIXEL, ledPin>(&led, 1);
   pinMode(buttonPin, INPUT_PULLUP);
-#ifdef ARDUINO_AVR_UNO
-  heatIndex = headIndexStart;
-#endif
+  // Set LED to off.
+  setLED();
 }
 
 void loop()
 {
-  if (digitalRead(buttonPin) != LOW && heatIndex == 0)
+  // If button pressed and system glow ready start glow.
+  if (buttonPressed() && glowFinished)
   {
-    heatIndex = headIndexStart;
+    startGlow();
   }
 
-  sunrise();
+  // Update glow colour based on time
+  glow();
   FastLED.show();
 }
 
-void sunrise()
+// True if button is currently pressed
+bool buttonPressed() {return digitalRead(buttonPin) != LOW;}
+// True if LED off and glow complete
+bool glowFinished() {return glowIndex == 0;}
+// start new glow sequence
+void startGlow()
 {
-  CRGB color = ColorFromPalette(HeatColors_p, heatIndex);
-  fill_solid(leds, NUM_LEDS, color);
-  
-  static const uint32_t sunriseLength = 20;  // seconds
-  static const uint32_t interval = (sunriseLength * 1000) / headIndexStart;
-
+  glowIndex = glowStart;
+  setLED();
+}
+// Set LED to current index colour
+void setLED()
+{
+    CRGB color = ColorFromPalette(HeatColors_p, glowIndex);
+    led = color;
+}
+ 
+// Update glow colour based on time
+void glow()
+{
   EVERY_N_MILLISECONDS(interval)
   {
-    if (heatIndex > 0)
+    if (glowIndex > 0)
     {
-      heatIndex--;
-#ifdef SERIAL_LOGGING
-      Serial.println(heatIndex);
-#endif
+      glowIndex--;
+      setLED();
     }
   }
 }
