be24c39c88db8eda707dac6537796d39a494d498
Dave Barnett <dave.barnett@redsensors.com>
2018-02-11 00:30:53 +0000

    Button Test

---
+++ buttonTest/buttonTest.ino
@@ -0,0 +1,28 @@
+#include "FastLED.h"
+
+#define NUM_LEDS 1
+#define DATA_PIN 0
+#define BUTTON_PIN 3
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(buttonPin) != LOW)
+  {
+    leds[0].red = 0;
+    leds[0].green = 0;
+    leds[0].blue = 255;
+  }
+  else
+  {
+    leds[0].red = 255;
+    leds[0].green = 0;
+    leds[0].blue = 0;
+  }
+  FastLED.show();
+}
