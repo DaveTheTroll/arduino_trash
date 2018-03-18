91f2d84bfea6e12d1b7914f64010c8937e9af260
Dave Barnett <dave.barnett@redsensors.com>
2018-03-18 20:04:38 +0000

    Fading in of pulse.

--- bpm/bpm/bpm.ino
+++ bpm/bpm/bpm.ino
@@ -5,23 +5,25 @@
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 
-int pulsePin = 11;                // Pulse Sensor purple wire connected to analog pin X
+const int pulsePin = 11;                // Pulse Sensor purple wire connected to analog pin X
 
 CRGB leds[NUM_LEDS];
 
-CRGB normal(32, 255, 32);
-CRGB high(255, 0, 0);
-int highThreshold = 100;
-int brighnessHi = 255;
-int brightnessLo = 16;
-int pulseLength = 50;
-int pulseFactor = 4;
+const CRGB normal(32, 255, 32);
+const CRGB high(255, 0, 0);
+const int highThreshold = 100;
+const int brightnessHi = 255;
+const int brightnessLo = 16;
+const int pulseLength = 250;
+const int pulseFactor = 4;
 
 volatile int BPM;                   // used to hold the pulse rate
 volatile int Signal;                // holds the incoming raw data
 volatile int IBI = 600;             // holds the time between beats, the Inter-Beat Interval
 volatile boolean Pulse = false;     // true when pulse wave is high, false when it's low
 volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+
+long next;
 
 void setup()
 {
@@ -34,22 +36,35 @@ void setup()
 // Uncomment to connect to pulse sensor.
 // Also include interrupt.ino from flora-pulse-neopixels
 //  delay(1000);                      // Wait a second
-//  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS 
+//  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
+
+  next = millis() + (pulseFactor*60000)/BPM;
 }
 
 void loop()
 {
   fill_solid(leds, NUM_LEDS, (BPM > highThreshold) ? high : normal);
-  FastLED.setBrightness(brighnessHi);
-  FastLED.delay(pulseLength);
-  FastLED.show();
-  FastLED.setBrightness(brightnessLo);
-  FastLED.delay((60000*pulseFactor / BPM) - pulseLength);
+
+  long timeUntilPulse = next - millis();
+
+  if (timeUntilPulse > pulseLength) // Pulse not started
+  {
+    FastLED.setBrightness(brightnessLo);
+  }
+  else if (timeUntilPulse > -pulseLength)  // Pulse on
+  {
+    FastLED.setBrightness(brightnessLo + ((brightnessHi - brightnessLo) * (pulseLength - abs(timeUntilPulse)))/pulseLength);
+  }
+  else  // Pulse complete
+  {
+    FastLED.setBrightness(brightnessLo);
+    next = next + (pulseFactor*60000)/BPM;  // Next pulse is at previous pulse + period.
+    Serial.println(next);
+  }
   FastLED.show();
 
   if (Serial.available() > 0)
   {
     BPM = Serial.parseInt();
   }
-
 }
